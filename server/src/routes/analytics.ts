b24ca1f11c1bef086d48760ad34f16cc499dc436
Sunil Panchani <git@techforbs.com>
2025-12-01 17:13:51 +0530

    Update analytics.ts

--- server/src/routes/analytics.ts
+++ server/src/routes/analytics.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import * as express from 'express';
+import type { Request, Response } from 'express';
 import {
   getAllPartnerConfigs,
   getPartnerConfig,
